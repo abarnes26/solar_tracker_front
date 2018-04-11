00355ff75a04f4a45539700495b2457bd159b410
abarnes26 <abarnes26@yahoo.com>
2018-04-10 21:35:38 -0600

    reorganized branch requests

---
+++ src/utils/branch_requests.js
@@ -0,0 +1,51 @@
+const postBranchHeaders = (branchStreet, branchCity, branchState, branchZipcode) => {
+  return {
+    method: `POST`,
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ branch: { street: branchStreet, city: branchCity, state: branchState, zipcode: branchZipcode }})
+  }
+}
+
+const updateBranchHeaders = (attr, value) => {
+  return {
+    method: `PATCH`,
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ branch: { attr: value }})
+  }
+}
+
+const deleteBranchHeaders = id => {
+  return {
+    method: `DELETE`,
+  }
+}
+
+const addBranch = (street, city, state, zipcode) => {
+  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches`, postHeaders(email, password))
+    .then((response) => handleResponse(response))
+    .catch((error) => console.error({ error }))
+}
+
+const getAllBranches = () => {
+  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches`)
+    .then((response) => handleResponse(response))
+    .catch((error) => console.error({ error }))
+}
+
+const getBranch = id => {
+  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches/${id}`)
+    .then((response) => handleResponse(response))
+    .catch((error) => console.error({ error }))
+}
+
+const updateBranch = (id, attr, value) => {
+  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches/${id}`, updateBranchHeaders(attr, value)
+    .then((response) => handleResponse(response))
+    .catch((error) => console.error({ error }))
+}
+
+const deleteBranch = id => {
+  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches/${id}`, deleteBranchHeaders()
+    .then((response) => handleResponse(response))
+    .catch((error) => console.error({ error }))
+}
--- src/utils/requests.js
+++ src/utils/requests.js
@@ -21,60 +21,8 @@ const postAccountHeaders = (userEmail, userPassword) => {
   }
 }
 
-const postBranchHeaders = (branchStreet, branchCity, branchState, branchZipcode) => {
-  return {
-    method: `POST`,
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({ branch: { street: branchStreet, city: branchCity, state: branchState, zipcode: branchZipcode }})
-  }
-}
-
-const updateBranchHeaders = (attr, value) => {
-  return {
-    method: `PATCH`,
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({ branch: { attr: value }})
-  }
-}
-
-const deleteBranchHeaders = id => {
-  return {
-    method: `DELETE`,
-  }
-}
-
 const addAccount = (email, password) => {
   return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/users`, postHeaders(email, password))
-    .then((response) => handleResponse(response))
-    .catch((error) => console.error({ error }))
-}
-
-const addBranch = (street, city, state, zipcode) => {
-  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches`, postHeaders(email, password))
-    .then((response) => handleResponse(response))
-    .catch((error) => console.error({ error }))
-}
-
-const getAllBranches = () => {
-  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches`)
-    .then((response) => handleResponse(response))
-    .catch((error) => console.error({ error }))
-}
-
-const getBranch = id => {
-  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches/${id}`)
-    .then((response) => handleResponse(response))
-    .catch((error) => console.error({ error }))
-}
-
-const updateBranch = id => {
-  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches/${id}`, updateBranchHeaders(attr, value)
-    .then((response) => handleResponse(response))
-    .catch((error) => console.error({ error }))
-}
-
-const deleteBranch = id => {
-  return fetch(`https://solar-carbon-tracker-api.herokuapp.com/api/v1/branches/${id}`, deleteBranchHeaders()
     .then((response) => handleResponse(response))
     .catch((error) => console.error({ error }))
 }
